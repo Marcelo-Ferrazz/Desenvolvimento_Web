eb290e8c7704a0580c103c10df7163e102c09474
Marcelo-Ferrazz <dferrazz@hotmail.com>
2022-10-10 21:38:18 -0300

    add aulas

---
+++ JS/03_String/main.js
@@ -0,0 +1,8 @@
+var string1 = "Falta a camisa pessoal"
+
+ console.log(string1[0])
+ console.log(string1.charAt(0))
+ console.log(string1.charAt(1))
+ console.log(string1.charAt(2))
+ console.log(string1.charAt(3))
+ console.log(string1.charAt(4))
